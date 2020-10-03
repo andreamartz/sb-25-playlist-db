375ed02e56cdd2e918d30790ab0435ea6b62b74c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-10-03 13:30:49 -0500

    Add form for songs

--- playlist-app/forms.py
+++ playlist-app/forms.py
@@ -20,7 +20,11 @@ class SongForm(FlaskForm):
     """Form for adding songs."""
 
     # Add the necessary code to use this form
+    title = StringField("Song Title",
+                        validators=[InputRequired(message="Song title cannot be blank.")])
 
+    artist = StringField("Artist", validators=[
+                         InputRequired(message="Artist cannot be blank.")])
 
 # DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
 class NewSongForPlaylistForm(FlaskForm):
